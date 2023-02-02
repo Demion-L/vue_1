b2e54cb51331eccb158c805c73d842b0df68a089
Demion-L <padovds@ukr.net>
2023-02-02 14:40:32 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <router-link to="/story">Story</router-link>
   </nav>
   <router-view/>
 </template>
